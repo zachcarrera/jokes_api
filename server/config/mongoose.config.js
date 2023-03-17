16feea1f058aa72dd99f17d76717499182a4354d
Zach Carrera <zachcarrera@gmail.com>
2023-03-17 07:25:34 -0700

    add mongoose and mongodb config

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const dbName = process.env.DB;
+const username = process.env.USERNAME;
+const pw = process.env.PASSWORD;
+const uri = `mongodb+srv://${username}:${pw}@cluster0.7fscy2n.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Established a connection to the database"))
+    .catch((err) =>
+        console.log("Something went wrong when connecting to the database", err)
+    );
