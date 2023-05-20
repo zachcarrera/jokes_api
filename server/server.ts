7f17e7cd19775f1a9c39cc39d72a1a6cb3054555
Zach Carrera <zachcarrera@gmail.com>
2023-05-19 23:31:53 -0700

    convert import syntax to es modules

--- server/server.ts
+++ server/server.ts
@@ -1,28 +1,18 @@
-import dotenv from "dotenv"
-import express, {Express, Request, Response} from "express"
-import { jokesRouter } from "./routes/jokes.routes";
-
-// require("dotenv").config();
+import dotenv from "dotenv";
 dotenv.config();
-// const express = require("express");
-// const { jokesRouter } = require("./routes/jokes.routes");
 
+import express, { Express, Request, Response } from "express";
+import { jokesRouter } from "./routes/jokes.routes";
+import { connect } from "./config/mongoose.config";
 
 const port = process.env.PORT;
-require("./config/mongoose.config");
+
+connect();
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const testData = {message: "hello"}
-
-app.get("/", (req, res) => {
-    return res.json(testData);
-})
-
-console.log(app)
 
 app.use("/api/jokes", jokesRouter);
 
